5475a79c6ee2f538c387486add916544cb48748c
fuxiangyang <fuxiangyang945@126.com>
2020-07-07 10:55:41 +0800

    build(deploy.sh): 调整push远程仓库位置
    构建完之后再进行提交,然后部署,否则部署后还是构建前的代码
    
    BREAKING CHANGE: 部署后还是展示的旧代码,需要在构建build之后在进行push,而非之前

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 提交到远程仓库-码云
-git push origin
-
-# 提交到远程仓库-github
-git push github
-
 # 生成静态文件
 tiddlywiki wiki --build index
 
 # 拷贝打包后新版本的文件 到 部署目录docs
 cp wiki/output/index.html docs/index.html
+
+# 提交到远程仓库-码云
+git push origin
+
+# 提交到远程仓库-github
+git push github
